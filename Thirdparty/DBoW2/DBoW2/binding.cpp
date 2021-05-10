28a185f744c8309aeb2d94a683fd38ec813b367a
Anatoly Belikov <awbelikov@gmail.com>
2021-05-10 13:07:52 +0300

    fix python binding

--- Thirdparty/DBoW2/DBoW2/binding.cpp
+++ Thirdparty/DBoW2/DBoW2/binding.cpp
@@ -18,6 +18,8 @@ typedef DBoW2::TemplatedDatabase<DBoW2::FGOOD::TDescriptor, DBoW2::FGOOD>
   GOODDatabase;
 
 
+PYBIND11_MAKE_OPAQUE(std::vector<DBoW2::Result>);
+
 PYBIND11_MODULE(DBoW, m) {
     NDArrayConverter::init_numpy();
 
@@ -33,15 +35,28 @@ PYBIND11_MODULE(DBoW, m) {
     .def_readwrite("Score", &DBoW2::Result::Score)
     .def_readwrite("nWords", &DBoW2::Result::nWords);
 
-    py::class_<DBoW2::QueryResults>(m, "QueryResults");
-    
+    py::class_<std::vector<DBoW2::Result> >(m, "ResultVector")
+        .def(py::init<>())
+        .def("clear", &std::vector<DBoW2::Result>::clear)
+        .def("pop_back", &std::vector<DBoW2::Result>::pop_back)
+        .def("__len__", [](const std::vector<DBoW2::Result> &v) { return v.size(); })
+        .def("__iter__", [](std::vector<DBoW2::Result> &v) {
+           return py::make_iterator(v.begin(), v.end());
+        }, py::keep_alive<0, 1>());
+
+    py::class_<DBoW2::QueryResults, std::vector<DBoW2::Result> >(m, "QueryResults");
+
     py::class_<GOODDatabase>(m, "GOODDatabase")
         .def(py::init<const GOODVocabulary &, bool, int>())
-        .def("add",   static_cast<DBoW2::EntryId (GOODDatabase::*)(const DBoW2::BowVector &, const DBoW2::FeatureVector&)> (&GOODDatabase::add), "add an item to DBoW",
-             py::arg("features"), py::arg("fec") = DBoW2::FeatureVector())
+        .def("add",   static_cast<DBoW2::EntryId (GOODDatabase::*)(const std::vector<GOODDatabase::TDesc> &)> (&GOODDatabase::add),
+			"add an item to DBoW\n \
+			features - list of cv::Mat, each cv::Mat 1x256 matrix",
+             py::arg("features"))
 
-        .def("query", static_cast<DBoW2::QueryResults (GOODDatabase::*)(const DBoW2::BowVector &, int, int) const>
-        (&GOODDatabase::query) , "query database with a feature matrix",
-             py::arg("vec"), py::arg("max_results")=1, py::arg("max_id")=-1);
+        .def("query", static_cast<DBoW2::QueryResults (GOODDatabase::*)(const std::vector<GOODDatabase::TDesc> &, int, int) const>
+	        (&GOODDatabase::query) , "query database with a feature matrix",
+             py::arg("features"), py::arg("max_results")=1, py::arg("max_id")=-1)
+
+	.def("size", &GOODDatabase::size);
 
 }
